fdfad1caf75c7b21d17fc640b95b5da30d72a20b
epburiti <50504484+epburiti@users.noreply.github.com>
2021-04-08 17:08:11 -0300

    feat: basic config

---
+++ src/styles/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    background: '#FFF',
+    text: '#A5A5A5A5',
+    textHome: '#FFF',
+    green: '#39b54a',
+    txtInput: '#484848',
+    cardInput: '#333'
+  }
+};
+
+export default theme;
